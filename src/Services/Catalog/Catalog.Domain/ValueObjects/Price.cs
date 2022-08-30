b216eb5f3fcd9d1eccc10eb675415e3ffd9ec8bc
Dimas de la Fuente <dimasdelafuen@gmail.com>
2022-08-31 01:05:30 +0200

    Configure Catalog architecture

---
+++ src/Services/Catalog/Catalog.Domain/ValueObjects/Price.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.Domain.Enumerations;
+using Catalog.Domain.Shared;
+
+namespace Catalog.Domain.ValueObjects;
+
+public class Price : ValueObject
+{
+    public decimal Amount { get; }
+    public Currency Currency { get; }
+
+    public Price(decimal amount, Currency currency)
+    {
+        Ensure.Argument.NotNull(amount, nameof(amount));
+        Ensure.Argument.NotNull(currency, nameof(currency));
+        Amount = amount;
+        Currency = currency;
+    }
+
+    public static Price From(decimal amount, string code)
+    {
+        return new Price(amount, Enumeration.FromDisplayName<Currency>(code));
+    }
+
+    protected override IEnumerable<object> GetAtomicValues()
+    {
+        yield return Amount;
+        yield return Currency;
+    }
+}
+
---
+++ src/Services/Catalog/Catalog.Infrastructure/Configurations/ProductConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.Domain.Entities;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.IdGenerators;
+
+namespace Catalog.Infrastructure.Configurations
+{
+    internal sealed class ProductConfiguration : IDbConfiguration<Product>
+    {
+        public void Register()
+        {
+            BsonClassMap.RegisterClassMap<Product>(cm => {
+                cm.AutoMap();
+                cm.MapIdField("id").SetIdGenerator(GuidGenerator.Instance);
+            });
+        }
+    }
+}
