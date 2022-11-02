e4604a74912a7e0392254a0c6defee358de16727
dimas.delafuente <dimas.delafuente@channeladvisor.com>
2022-11-02 17:36:01 +0100

    Create Discount.Grpc project

---
+++ src/Services/Discount/Discount.Grpc/Mappers/ProductDiscountMapper.cs
@@ -0,0 +1,14 @@
+﻿using ProductDiscount = Discount.Grpc.Protos.ProductDiscount;
+
+namespace Discount.Grpc.Mappers;
+
+public static class ProductDiscountMapper
+{
+    public static ProductDiscount ToProtoModel(this Domain.Entities.ProductDiscount productDiscount)
+        => new()
+        {
+            ProductId = productDiscount.ProductId.ToString(),
+            Amount = Convert.ToDouble(productDiscount.Amount),
+            Description = productDiscount.Description
+        };
+}
---
+++ src/Services/Discount/Discount.Grpc/Services/ProductDiscountService.cs
@@ -0,0 +1,62 @@
+﻿using Discount.Application.Features.ProductDiscounts.Commands.CreateProductDiscount;
+using Discount.Application.Features.ProductDiscounts.Commands.DeleteProductDiscount;
+using Discount.Application.Features.ProductDiscounts.Commands.UpdateProductDiscount;
+using Discount.Application.Features.ProductDiscounts.Queries.GetProductDiscount;
+using Discount.Grpc.Mappers;
+using Discount.Grpc.Protos;
+using Grpc.Core;
+using MediatR;
+
+namespace Discount.Grpc.Services;
+
+public class ProductDiscountService : ProductDiscountProtoService.ProductDiscountProtoServiceBase
+{
+    private readonly ISender _mediator;
+
+    public ProductDiscountService(ISender mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public override async Task<ProductDiscount> GetProductDiscount(GetProductDiscountRequest request, ServerCallContext context)
+    {
+        var result = await _mediator.Send(new GetProductDiscountQuery(Guid.Parse(request.ProductId)));
+
+        return result.IsError
+            ? throw new RpcException(new Status(StatusCode.NotFound, result.FirstError.Description))
+            : result.Value.ProductDiscount.ToProtoModel();
+    }
+
+    public override async Task<ProductDiscount> CreateProductDiscount(CreateProductDiscountRequest request, ServerCallContext context)
+    {
+        var command = new CreateProductDiscountCommand(Guid.Parse(request.ProductDiscount.ProductId), request.ProductDiscount.Description, Convert.ToDecimal(request.ProductDiscount.Amount));
+        var result = await _mediator.Send(command);
+
+        return result.IsError
+            ? throw new RpcException(new Status(StatusCode.Internal, result.FirstError.Description))
+            : result.Value.ProductDiscount.ToProtoModel();
+    }
+
+    public override async Task<ProductDiscount> UpdateProductDiscount(UpdateProductDiscountRequest request, ServerCallContext context)
+    {
+        var command = new UpdateProductDiscountCommand(Guid.Parse(request.ProductDiscount.ProductId), request.ProductDiscount.Description, Convert.ToDecimal(request.ProductDiscount.Amount));
+        var result = await _mediator.Send(command);
+
+        return result.IsError
+            ? throw new RpcException(new Status(StatusCode.Internal, result.FirstError.Description))
+            : request.ProductDiscount;
+    }
+
+    public override async Task<DeleteProductDiscountResponse> DeleteProductDiscount(DeleteProductDiscountRequest request, ServerCallContext context)
+    {
+        var command = new DeleteProductDiscountCommand(Guid.Parse(request.ProductId));
+        var result = await _mediator.Send(command);
+
+        return result.IsError
+            ? throw new RpcException(new Status(StatusCode.Internal, result.FirstError.Description))
+            : new DeleteProductDiscountResponse
+            {
+                Deleted = result.Value.Deleted
+            };
+    }
+}
